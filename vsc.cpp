812822c374e7319949f9cfbe32af1503fa29be83
ayatarek831 <64319299+ayatarek831@users.noreply.github.com>
2023-02-17 14:02:28 +0200

    minimum function

--- vsc.cpp
+++ vsc.cpp
@@ -12,9 +12,22 @@ int getSum(std::vector<int> arr)
     return sum;
 }
 
+int getMin(std::vector<int> arr)
+{
+    int length = arr.size();
+    int min=arr[0];
+    for(int i =1; i < length;i++)
+    {
+        if(min> arr[i])
+        min = arr[i];
+    }
+    return min;
+}
+
 int main()
 {
     std::vector<int> arr ={1,2,3,4,5,6,7,8,9,10};
     std::cout<<"The sum of the vector is: "<<getSum(arr)<<std::endl;
+    std::cout<<"The min of the vector is: "<<getMin(arr)<<std::endl;
     return 0;
 }
